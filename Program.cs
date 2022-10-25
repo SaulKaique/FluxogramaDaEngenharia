222691eee56c8c4eb62ab6836405bc490ad4b56c
SaulKaique <Saul.silva@etec.sp.gov.br>
2022-10-25 17:57:42 -0300

    Finalizado e completo dos pedidos

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("--- Fluxograma Da Engenharia --- \n");
+Console.ResetColor();
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("O objeto esta se movendo:(S/N):");
+bool vendoEstaSemo = Console.ReadLine()! .Trim().Substring(0,1).ToUpper()== "S";
+
+
+Console.Write("Deveria?");
+bool Deveria = Console.ReadLine()! .Trim().Substring(0,1).ToUpper()== "S";
+
+Console.ResetColor();
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+
+
+string mensagem;
+
+if (!vendoEstaSemo)
+{
+    if (!Deveria)
+    {
+        mensagem = "Ótimo!";
+    }
+    else
+    {
+        mensagem = "Use WD-40.";
+    }
+}
+else
+{
+    if (Deveria)
+    {
+        mensagem = "Ótimo!";
+    }
+    else
+    {
+        mensagem = "Use Silver Tape.";
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(mensagem);
+
+Console.ResetColor();
